da7c73421ac4b34bf8bb10a4f9403e9dfca4a59b
Kosuke <bkpc9070@yahoo.ne.jp>
2021-09-17 21:45:43 +0900

    完成 9/17

--- js/style.js
+++ js/style.js
@@ -27,3 +27,27 @@ $(function(){
    });
 });
 
+
+// 要素の高さを取得
+$(function() {
+$(window).load(function(){
+  // タイトルのBOX分の高さを取得
+  var child1 = $('div[class="title_box"]').height();
+  // その下のコンテンツのBOX分の高さを取得
+  var child2 = $('div[class="menu_box"]').outerHeight();
+  var child3 = $('div[class="contents_wrapper_inner2"]').outerHeight();
+  var child4 = $('div[class="location_box"]').outerHeight();
+
+
+  // 余白分の高さを計算
+  var mhsize = child1 + child2;
+  var mhsize2 = child1 + child3;
+  var mhsize3 = child1 + child4;
+  // cssにその高さを反映させる
+  $('div[class*="margin_red"]').css("height", mhsize + "px");
+  $('div[class*="margin_red2"]').css("height", mhsize2 + "px");
+  $('div[class*="margin_red3"]').css("height", mhsize3 + "px");
+
+
+});
+});
