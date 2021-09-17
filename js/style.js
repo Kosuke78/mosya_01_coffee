aaea23975e99b84a45d4fd46e9fb1f55b8121ace
Kosuke <bkpc9070@yahoo.ne.jp>
2021-09-17 22:28:07 +0900

    完成 9/17

--- js/style.js
+++ js/style.js
@@ -10,22 +10,22 @@ $(window).resize(function () {
 }); 
 
 // スムーズスクロール
-// $(function(){
+ $(function(){
    // #で始まるアンカーをクリックした場合に処理
-//   $('a[href^=#]').click(function() {
+   $('a[href^=#]').click(function() {
       // スクロールの速度
-  //    var speed = 400; // ミリ秒
+      var speed = 400; // ミリ秒
       // アンカーの値取得
-    //  var href= $(this).attr("href");
+      var href= $(this).attr("href");
       // 移動先を取得
-    //  var target = $(href == "#" || href == "" ? 'html' : href);
+      var target = $(href == "#" || href == "" ? 'html' : href);
       // 移動先を数値で取得
-    //  var position = target.offset().top;
+      var position = target.offset().top;
       // スムーススクロール
-    //  $('body,html').animate({scrollTop:position}, speed, 'swing');
-    //  return false;
- //  });
-// });
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+ });
 
 
 // 要素の高さを取得
