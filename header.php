31e44d5449ebb18c21c98e9f003f7dfb41a0735c
Kosuke <bkpc9070@yahoo.ne.jp>
2021-09-18 17:47:12 +0900

    wp化しました

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/style.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+	</header>
+	<div class="main">
---
+++ js/style.js
@@ -0,0 +1,54 @@
+// 画面幅に画像を表示
+$(document).ready(function () {
+   hsize = $(window).height();
+   $('div[class*="mv"]').css("height", hsize + "px");
+ });
+ $(window).resize(function () {
+   hsize = $(window).height();
+   $('div[class*="mv"]').css("height", hsize + "px");
+   8;
+ }); 
+ 
+ // スムーズスクロール
+  $(function(){
+    // #で始まるアンカーをクリックした場合に処理
+    $('a[href^="#"]').click(function() {
+       // スクロールの速度
+       var speed = 400; // ミリ秒
+       // アンカーの値取得
+       var href= $(this).attr("href");
+       // 移動先を取得
+       var target = $(href == "#" || href == "" ? 'html' : href);
+       // 移動先を数値で取得
+       var position = target.offset().top;
+       // スムーススクロール
+       $('body,html').animate({scrollTop:position}, speed, 'swing');
+       return false;
+    });
+  });
+ 
+ 
+ // 要素の高さを取得
+ $(function() {
+ $(window).on('load',function(){
+   // タイトルのBOX分の高さを取得
+   var child1 = $('div[class="title_box"]').height();
+   // その下のコンテンツのBOX分の高さを取得
+   var child2 = $('div[class="menu_box"]').outerHeight();
+   var child3 = $('div[class="contents_wrapper_inner2"]').outerHeight();
+   var child4 = $('div[class="location_box"]').outerHeight();
+ 
+ 
+   // 余白分の高さを計算
+   var mhsize = child1 + child2;
+   var mhsize2 = child1 + child3;
+   var mhsize3 = child1 + child4;
+   // cssにその高さを反映させる
+   $('div[class*="margin_red"]').css("height", mhsize + "px");
+   $('div[class*="margin_red2"]').css("height", mhsize2 + "px");
+   $('div[class*="margin_red3"]').css("height", mhsize3 + "px");
+ 
+ 
+ });
+ });
+ 
