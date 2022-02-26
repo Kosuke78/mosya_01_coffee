2e39c0a531ce2b58b6befac9c1698263743dfca0
Kosuke <bkpc9070@yahoo.ne.jp>
2022-02-27 00:46:35 +0900

    2022/02/26リファクタ

--- js/style.js
+++ js/style.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   hsize = $(window).height();
   $('.mv').css("height", hsize + "px");
 });
-$(window).resize(function () {
+$(function () {
   hsize = $(window).height();
   $('.mv').css("height", hsize + "px");
   8;
@@ -31,6 +31,9 @@ $(window).resize(function () {
 // 要素の高さを取得
 
 
+});
+
+$(function(){
   // タイトルのBOX分の高さを取得
   var child1 = $('.title_box').height();
   // その下のコンテンツのBOX分の高さを取得
@@ -49,5 +52,25 @@ $(window).resize(function () {
   $('.margin_red3').css("height", mhsize3 + "px");
 
 
+})
+$(window).resize(function(){
+  hsize = $(window).height();
+  $('.mv').css("height", hsize + "px");
 
-});
+  // タイトルのBOX分の高さを取得
+  var child1 = $('.title_box').height();
+  // その下のコンテンツのBOX分の高さを取得
+  var child2 = $('.menu_box').outerHeight();
+  var child3 = $('.contents_wrapper_inner2').outerHeight();
+  var child4 = $('.location_box').outerHeight();
+
+
+  // 余白分の高さを計算
+  var mhsize = child1 + child2;
+  var mhsize2 = child1 + child3;
+  var mhsize3 = child1 + child4;
+  // cssにその高さを反映させる
+  $('.margin_red').css("height", mhsize + "px");
+  $('.margin_red2').css("height", mhsize2 + "px");
+  $('.margin_red3').css("height", mhsize3 + "px");
+})
